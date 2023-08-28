a436c4292a0a377856d5f2face69922d845d2ad3
Eran Geva <eran.geva@gmail.com>
2023-08-29 00:43:37 +0300

    CombineTranslatedPdf not working yet

--- MergePdfImages.py
+++ MergePdfImages.py
@@ -21,7 +21,7 @@ def hconcat(images):
     for im in images:
         new_im.paste(im, (offsets[-1], 0))
         offsets.append(offsets[-1] + im.size[0])
-    return new_im
+    return new_im, offsets
 
 def vconcat(images):
     widths, heights = zip(*(i.size for i in images))
@@ -30,7 +30,7 @@ def vconcat(images):
     for im in images:
         new_im.paste(im, (0, offsets[-1]))
         offsets.append(offsets[-1] + im.size[1])
-    return new_im
+    return new_im, offsets
 
 def partition(lst, n):
     return [lst[i:i + n] for i in range(0, len(lst), n)]
@@ -40,9 +40,23 @@ def flatten (l):
 
 def tileImages(images, numPerRow):
     p = partition(images, numPerRow)
-    rows = [hconcat(image_in_row) for image_in_row in p]
-    im = vconcat(rows)
-    return im
+
+    rows = []
+    x_offsets = []
+    for image_in_row in p:
+        imrow, x_offsets_curr = hconcat(image_in_row)
+        rows.append(imrow)
+        x_offsets.append(x_offsets_curr)
+
+    im, y_offsets = vconcat(rows)
+    (w, h) = im.size
+
+    rects = []
+    for y0, y1, xs in zip(y_offsets, y_offsets[1:] + [h], x_offsets):
+        for x0, x1 in zip(xs, xs[1:] + [w]):
+            rects.append((x0, y0, x1, y1))
+
+    return im, rects
     
 def MergePdfImages(pdfFileNames, output_folder=None):
     if output_folder is None:
@@ -51,16 +65,62 @@ def MergePdfImages(pdfFileNames, output_folder=None):
 
     images_for_pdfs = [convert_from_path(pdf) for pdf in pdfFileNames]
     images = flatten(images_for_pdfs)
-    rows = 3
-    cols = 3
+    rows = 2
+    cols = 2
     p = partition(images, cols * rows)
     for i, images_in_tile in enumerate(p):
-        im = tileImages(images_in_tile, cols)
+        im, rects = tileImages(images_in_tile, cols)
         name = join(output_folder, 'im_' + '_' + str(i) + '.png')
+        print('on ', name, ' got ', rects)
         im.save(name)
+            
+def CombineTranslatedPdf(pdfFileNames, input_orig_images=None, input_translated_images=None, output_pdfs=None):
+    if input_orig_images is None:
+        input_orig_images = join(os.getcwd(), 'merged')
+    if input_translated_images is None:
+        input_translated_images = join(os.getcwd(), 'translated')
+    if output_pdfs is None:
+        output_pdfs = join(os.getcwd(), 'output_pdfs')
+
+    makeEmptyDir(output_pdfs)
+    temp_folder = makeEmptyDir(join(output_pdfs, 'temp'))
+
+    images_for_pdfs = [convert_from_path(pdf) for pdf in pdfFileNames]
+    images = flatten(images_for_pdfs)
+    rows = 2
+    cols = 2
+    index = 0
+    p = partition(images, cols * rows)
+    for i, images_in_tile in enumerate(p):
+        im, rects = tileImages(images_in_tile, cols)
+        name = join(input_orig_images, 'im__' + str(i) + '.png')
+        print('on ', name, ' got ', rects)
+
+        name_trans = join(input_translated_images, 'im__' + str(i) + '.png')
+        im_trans = Image.open(name_trans)
+        
+        for (x0, y0, x1, y1) in rects:
+            im0 = im.crop((x0, y0, x1, y1))
+            im1 = im_trans.crop((x0, y0, x1, y1))
+            imboth, _ = hconcat([im0, im1])
+            name_temp = join(temp_folder, 'im_' + str(index) + '.jpg')
+            index += 1
+            imboth.save(name_temp)
+        # im.save(name)
+        # imr, _ = hconcat([im, im_trans])
+        # imr.save(name_temp)
+        
+            
+
 
 if __name__ == '__main__':
-    MergePdfImages([
+    # MergePdfImages([
+    #     '/home/eran/Downloads/Adobe Scan Aug 28, 2023.pdf', 
+    #     '/home/eran/Downloads/Adobe Scan Aug 28, 2023 (1).pdf', 
+    #     '/home/eran/Downloads/Adobe Scan Aug 28, 2023 (3).pdf', 
+    #     '/home/eran/Downloads/Adobe Scan Aug 28, 2023 (2).pdf', 
+    #     ])
+    CombineTranslatedPdf([
         '/home/eran/Downloads/Adobe Scan Aug 28, 2023.pdf', 
         '/home/eran/Downloads/Adobe Scan Aug 28, 2023 (1).pdf', 
         '/home/eran/Downloads/Adobe Scan Aug 28, 2023 (3).pdf', 
